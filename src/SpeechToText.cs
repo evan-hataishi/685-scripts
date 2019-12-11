9b207959107e56da4d09dfef7846e6bd7cd10375
Evan <evanhata@hawaii.edu>
2019-12-11 11:31:22 -1000

    Add rest calls to stt

--- src/SpeechToText.cs
+++ src/SpeechToText.cs
@@ -3,39 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Windows.Speech;
-
+using UnityEngine.Networking;
+using static System.String;
 
 public class SpeechToText : MonoBehaviour
 {
-    [SerializeField]
-    private Text m_Hypotheses;
+    private float loopTime = 5.0f;
 
-    [SerializeField]
-    private Text m_Recognitions;
+    private string resultText;
 
-    private string currText;
+    private string hypothesisText;
 
     private DictationRecognizer m_DictationRecognizer;
 
-    private readonly Object myLock = new Object();
+    private readonly UnityEngine.Object myLock = new Object();
+
+    private string scoreAddr = "localhost:8080/api/score";
 
     void Start()
     {
         m_DictationRecognizer = new DictationRecognizer();
-        m_DictationRecognizer.AutoSilenceTimeoutSeconds = 100;
-        m_DictationRecognizer.InitialSilenceTimeoutSeconds = 100;
+        m_DictationRecognizer.InitialSilenceTimeoutSeconds = 600;
+        m_DictationRecognizer.AutoSilenceTimeoutSeconds = 600;
 
         //m_Hypotheses = new Text();
         //m_Recognitions = new Text();
-        currText = "";
+        resultText = "";
+        hypothesisText = "";
 
         m_DictationRecognizer.DictationResult += (text, confidence) =>
         {
             //Debug.LogFormat("Dictation result: {0}", text);
             // m_Recognitions.text += text + "\n";
-            lock(myLock)
+            //print("found a result");
+            lock (myLock)
             {
-                currText += text + "\n";
+                resultText += text + " ";
             }
         };
 
@@ -43,12 +46,23 @@ public class SpeechToText : MonoBehaviour
         {
             //Debug.LogFormat("Dictation hypothesis: {0}", text);
             // m_Hypotheses.text += text;
+            print("here");
+            lock (myLock)
+            {
+                hypothesisText = text;
+            }
         };
 
         m_DictationRecognizer.DictationComplete += (completionCause) =>
         {
+            //print("in complete");
+            /**
             if (completionCause != DictationCompletionCause.Complete)
-                Debug.LogErrorFormat("Dictation completed unsuccessfully: {0}.", completionCause);
+                print(completionCause);
+            */
+            //Debug.LogErrorFormat("Dictation completed unsuccessfully: {0}.", completionCause);
+            m_DictationRecognizer.Start();
+
         };
 
         m_DictationRecognizer.DictationError += (error, hresult) =>
@@ -65,21 +79,73 @@ public class SpeechToText : MonoBehaviour
     {
         while (true)
         {
-            yield return new WaitForSeconds(5.0f);
-            lock(myLock)
+            yield return new WaitForSeconds(loopTime);
+            lock (myLock)
             {
-                string[] splitText = currText.Split(' ');
-                print(splitText.Length);
-                print(currText);
-                currText = "";
+                if (resultText != "")
+                {
+                    print(resultText);
+                }
+                else
+                {
+                    print(hypothesisText);
+                }
+                resultText = "";
+                hypothesisText = "";
             }
+            StartCoroutine(CalculateSpeechScore(hypothesisText));
         }
     }
 
     void Update()
     {
         // m_DictationRecognizer.Start();
-    }string
+    }
 
-    void getMaxSizeClip()
+    // Make sure text is not null
+    IEnumerator CalculateSpeechScore(string text)
+    {
+        float score = 0.0f;
+        string uri = scoreAddr + ToQueryParameter(text);
+
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            //string[] pages = uri.Split('/');
+            //int page = pages.Length - 1;
+
+            if (webRequest.isNetworkError)
+            {
+                //Debug.Log(pages[page] + ": Error: " + webRequest.error);
+                print("Got error");
+            }
+            else
+            {
+                //Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                try
+                {
+                    score = float.Parse(webRequest.downloadHandler.text);
+                }
+                catch
+                {
+                    print("Parse float exception");
+                }
+            }
+            ProcessSpeechScore(score);
+        }
+    }
+
+    string ToQueryParameter(string text)
+    {
+        string[] split = text.Split(' ');
+        string joined = Join("_", split);
+        return "?phrase=" + joined;
+    }
+
+    void ProcessSpeechScore(float score)
+    {
+        print(score);
+    }
 }
